39f538e4acb9bb8c49e74c5471aef7f10a501f0d
AngelB <dinosaurio_adrian@hotmail.com>
2022-04-24 01:22:27 -0500

    actualizado a puerto 8080

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const app = express();
 const morgan = require('morgan');
 
 // settings
-app.set('port', process.env.PORT || 80)
+app.set('port', process.env.PORT || 8080)
 app.set('json spaces', 2)
 
 // middlewares
